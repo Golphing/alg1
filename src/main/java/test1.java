9cf50163ed00f4f2c4fd455927d7f2217cbf26d7
汪高平 <hzwanggaoping>
2023-11-19 21:36:46 +0800

    first commit

---
+++ src/main/java/test1.java
@@ -0,0 +1,12 @@
+import java.util.Vector;
+
+/**
+ * @author Golphing.W
+ * @date 2023/11/19 09:56
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        vector<int> a(9,0);
+    }
+}
