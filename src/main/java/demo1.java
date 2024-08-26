b062504362e7386519282a14597700cc40043a11
Golphing <hzwanggaoping@corp.netease.com>
2024-08-26 15:38:00 +0800

    add

--- src/main/java/demo1.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author Golphing.W
- * @date 2024/6/23 21:53
- */
-public class demo1 {
-    1
-}
---
+++ src/main/java/netease/easy/Demo.java
@@ -0,0 +1,9 @@
+package netease.easy;
+
+/**
+ * 记录易错点
+ * 记录不记得的API
+ *
+ */
+public class Demo {
+}
